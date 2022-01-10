075dc4e51becd21cced7edb0d15a63ac28b8725e
Satheesh <satheeshcharon@gmail.com>
2022-01-10 22:12:15 +0530

    Add files via upload

---
+++ wifi-passwords.py
@@ -0,0 +1,27 @@
+
+### GET SAVED WIFI PASSWORDS IN WINDOWS SYSTEM
+
+import subprocess , platform
+
+### GET SAVED PROFILES
+
+if platform.system() == 'Windows':
+    data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8', errors="backslashreplace").split('\n')
+
+
+    profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
+
+    ### LOOPING EACH PROFILE TO GET PASSWORDS
+    for profile in profiles:
+        try:
+            results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', profile, 'key=clear']).decode('utf-8', errors="backslashreplace").split('\n')
+            results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+            try:
+                print ("{:<30}|  {:<}".format(profile, results[0]))
+            except IndexError:
+                print ("{:<30}|  {:<}".format(profile, ""))
+        except subprocess.CalledProcessError:
+            print ("{:<30}|  {:<}".format(profile, "ENCODING ERROR"))
+
+else:
+    print('<<<<<<<<<<<<<<<<<<<<<<<<<   This is not the windows platform  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
